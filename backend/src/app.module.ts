d3f648aaa9400b983fb7c6c10d882b9fe6405d02
mircocosov <147343923+mircocosov@users.noreply.github.com>
2026-03-02 05:38:28 +0300

    fix(backend): provide PrismaService via PrismaModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma.service';
 import { LobbyModule } from './lobby/lobby.module';
 import { AdminModule } from './admin/admin.module';
 import { ChatModule } from './chat/chat.module';
 import { SceneModule } from './scene/scene.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, LobbyModule, AdminModule, ChatModule, SceneModule],
-  providers: [PrismaService]
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, LobbyModule, AdminModule, ChatModule, SceneModule]
 })
 export class AppModule {}
